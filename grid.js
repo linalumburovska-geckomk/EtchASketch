66c0fbcaab444f65cf7b05231014134705abf170
linalumburovska <linalumburovska20@gmail.com>
2019-01-29 16:20:40 +0100

    Added ability for changing color

--- grid.js
+++ grid.js
@@ -1,16 +1,20 @@
 var create=document.getElementById("create");
 var reset=document.getElementById("reset");
 var container=document.getElementById("container");
+var rainbow=document.getElementById("rainbow");
 var size=16;
+var cellColor="black";
 
 create.addEventListener('click', createGrid);
 reset.addEventListener('click',resetGrid);
+rainbow.addEventListener('click',changeColor);
 
 function createGrid(){
 
     size=window.prompt("Enter size for grid(default is 16)", "16");
     create.style.display="none";
     container.style.display="block";
+    rainbow.style.display="inline-block"
     
     for(var i = 0 ; i < size*size; i++){
         const cell = document.createElement('div');
@@ -23,7 +27,7 @@ function createGrid(){
         
                
         cell.addEventListener('mouseover',function(){
-                cell.style.backgroundColor = "black"; 
+                cell.style.backgroundColor = cellColor;
         })
         container.appendChild(cell); 
     }
@@ -36,4 +40,14 @@ function resetGrid(){
         container.removeChild(container.firstChild);
     }
     container.style.display="none";
+    rainbow.style.display="none";
+    cellColor="black";
+}
+
+function changeColor(){
+    // var red=Math.floor((Math.random() * 256));
+    // var green=Math.floor((Math.random() * 256)); 
+    // var blue=Math.floor((Math.random() * 256));
+
+    cellColor="yellow";
 }
