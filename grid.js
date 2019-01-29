86020a6f2a09ba4ab208bfd93a5485344f41b8b4
linalumburovska <linalumburovska20@gmail.com>
2019-01-29 15:30:44 +0100

    Added ability to reset the grid

--- grid.js
+++ grid.js
@@ -4,12 +4,15 @@ var container=document.getElementById("container");
 var size=16;
 
 create.addEventListener('click', createGrid);
+reset.addEventListener('click',resetGrid);
 
 function createGrid(){
-    
 
-    for(i = 0 ; i < size*size; i++)
-    {
+    size=window.prompt("Enter size for grid(default is 16)", "16");
+    create.style.display="none";
+    container.style.display="block";
+    
+    for(var i = 0 ; i < size*size; i++){
         const cell = document.createElement('div');
         cell.classList.add('cell');
         var hW=700/size;
@@ -24,4 +27,12 @@ function createGrid(){
         container.appendChild(cell); 
     }
 
+}
+
+function resetGrid(){
+    create.style.display="";
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    container.style.display="none";
 }
