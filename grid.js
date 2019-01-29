d84af03d51dec26823c8d43763101833ad9ce964
linalumburovska <linalumburovska20@gmail.com>
2019-01-29 15:01:05 +0100

    Painting cells on different sizes working but only with one color

--- grid.js
+++ grid.js
@@ -1,11 +1,27 @@
 var create=document.getElementById("create");
 var reset=document.getElementById("reset");
-
+var container=document.getElementById("container");
+var size=16;
 
 create.addEventListener('click', createGrid);
 
 function createGrid(){
     
-    
+
+    for(i = 0 ; i < size*size; i++)
+    {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        var hW=700/size;
+       
+        cell.style.height=hW+'px';
+        cell.style.width=hW+'px';
+        
+               
+        cell.addEventListener('mouseover',function(){
+                cell.style.backgroundColor = "black"; 
+        })
+        container.appendChild(cell); 
+    }
 
 }
