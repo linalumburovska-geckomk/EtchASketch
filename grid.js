35a2ad2531f34dee46e505f12a7a3cf66c84821c
linalumburovska <linalumburovska20@gmail.com>
2019-01-29 14:03:31 +0100

    Added README.md and first prototype of the application

---
+++ grid.js
@@ -0,0 +1,11 @@
+var create=document.getElementById("create");
+var reset=document.getElementById("reset");
+
+
+create.addEventListener('click', createGrid);
+
+function createGrid(){
+    
+    
+
+}
